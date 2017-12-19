c3cb836f54fda30b3b8dabca3bb0617544b2d278
EPaelinck@gmail.com <epaelinck@gmail.com>
2017-12-19 12:59:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
     return ("I can\'t hear you!")
   }
 
-  else if (string.toUpperCase === string) {
+  else if (string.toUpperCase() === string) {
     return ("YES INDEED!")
   }
 
